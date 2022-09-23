f8658ad6c7a612bc2c94a7c5c54218d25ccee859
chetan prakash udhane <78649010+chetanudhane@users.noreply.github.com>
2022-09-23 20:17:36 +0530

    Part time Employee Added

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -2,15 +2,21 @@ package day9.employee_wages_problem;
 
 public class EmployeeWages {
 
-	public static final int emp_Is_Present = 1;
+	public static final int emp_Is_Fulltime = 1;
+	public static final int emp_Is_Parttime = 2;
+	public static final int fulltime_Working_Hrs = 8;
+	public static final int parttime_Working_Hrs = 4;
 
 	public int checkAttendance() {
 
 		int working_Hrs;
-		int empRandomCheck = (int) (Math.random() * 2);
-		if (empRandomCheck == emp_Is_Present) {
-			System.out.println("Employee is Present");
-			working_Hrs = 8;
+		int empRandomCheck = (int) (Math.random() * 3);
+		if (empRandomCheck == emp_Is_Fulltime) {
+			System.out.println("Employee is Present Fulltime");
+			working_Hrs = fulltime_Working_Hrs;
+		} else if (empRandomCheck == emp_Is_Parttime) {
+			System.out.println("Employee is Present Parttime");
+			working_Hrs = parttime_Working_Hrs;
 		} else {
 			System.out.println("Employee is Absent");
 			working_Hrs = 0;
