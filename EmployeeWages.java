b2a197d06298586589cfeac27309b584ad0e2d6e
chetan prakash udhane <78649010+chetanudhane@users.noreply.github.com>
2022-09-23 20:24:29 +0530

    Using Switch Case Added

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -6,19 +6,44 @@ public class EmployeeWages {
 	public static final int emp_Is_Parttime = 2;
 	public static final int fulltime_Working_Hrs = 8;
 	public static final int parttime_Working_Hrs = 4;
+	private static final int EMP_IS_PART_TIME_PRESENT = 0;
 
-	public int checkAttendance() {
+//	public int checkAttendance() {
+//
+//		int working_Hrs;
+//		int empRandomCheck = (int) (Math.random() * 3);
+//		if (empRandomCheck == emp_Is_Fulltime) {
+//			System.out.println("Employee is Present Fulltime");
+//			working_Hrs = fulltime_Working_Hrs;
+//		} else if (empRandomCheck == emp_Is_Parttime) {
+//			System.out.println("Employee is Present Parttime");
+//			working_Hrs = parttime_Working_Hrs;
+//		} else {
+//			System.out.println("Employee is Absent");
+//			working_Hrs = 0;
+//		}
+//		return working_Hrs;
+//	}
+
+	public int switchCase() {
 
 		int working_Hrs;
-		int empRandomCheck = (int) (Math.random() * 3);
-		if (empRandomCheck == emp_Is_Fulltime) {
-			System.out.println("Employee is Present Fulltime");
+		int rand = (int) (Math.random() * 3);
+
+		switch (rand) {
+
+		case emp_Is_Fulltime:
+			System.out.println("Employee is full time Present");
 			working_Hrs = fulltime_Working_Hrs;
-		} else if (empRandomCheck == emp_Is_Parttime) {
-			System.out.println("Employee is Present Parttime");
+			break;
+
+		case EMP_IS_PART_TIME_PRESENT:
+			System.out.println("Employee is part time Present");
 			working_Hrs = parttime_Working_Hrs;
-		} else {
-			System.out.println("Employee is Absent");
+			break;
+
+		default:
+			System.out.println("Employee is absent");
 			working_Hrs = 0;
 		}
 		return working_Hrs;
@@ -26,7 +51,7 @@ public class EmployeeWages {
 
 	public void calculateDailyWages() {
 
-		int working_Hrs_PerDay = checkAttendance();
+		int working_Hrs_PerDay = switchCase();
 		int wage_Per_Hrs = 20;
 
 		int dailyWage = wage_Per_Hrs * working_Hrs_PerDay;
