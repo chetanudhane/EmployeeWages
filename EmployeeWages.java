dfb3d9ea753816424b0c0c0cc61cde89b6648326
chetan prakash udhane <78649010+chetanudhane@users.noreply.github.com>
2022-09-23 20:59:23 +0530

    Monthly Wage Added

---
+++ EmployeeWages.java
@@ -0,0 +1,53 @@
+package day9.employee_wages_problem;
+
+public class EmployeeWages {
+
+	public static final int emp_Is_Fulltime = 1;
+	public static final int emp_Is_Parttime = 2;
+	public static final int fulltime_Working_Hrs = 8;
+	public static final int parttime_Working_Hrs = 4;
+	public static final int working_Days_For_Month = 20;
+
+	public int checkAttendance() {
+
+		int working_Hrs;
+		int empRandomCheck = (int) (Math.random() * 3);
+		if (empRandomCheck == emp_Is_Fulltime) {
+			System.out.println("Employee is Present Fulltime");
+			working_Hrs = fulltime_Working_Hrs;
+		} else if (empRandomCheck == emp_Is_Parttime) {
+			System.out.println("Employee is Present Parttime");
+			working_Hrs = parttime_Working_Hrs;
+		} else {
+			System.out.println("Employee is Absent");
+			working_Hrs = 0;
+		}
+		return working_Hrs;
+	}
+
+	public int calculateDailyWages() {
+
+		int working_Hrs_PerDay = checkAttendance();
+		int wage_Per_Hrs = 20;
+
+		int dailyWage = wage_Per_Hrs * working_Hrs_PerDay;
+		System.out.println("Daily Wages For Employee Is : " + dailyWage);
+
+		return dailyWage;
+
+	}
+
+	public void calculateMonthlyWages() {
+		int monthly_Wage = 0;
+		for (int i = 1; i <= working_Days_For_Month; i++) {
+
+			System.out.println("Day-" + i);
+
+			monthly_Wage += calculateDailyWages();
+
+		}
+		System.out.println("Monthly Wage Of Employee : " + monthly_Wage);
+
+	}
+
+}
--- EmployeeWagesMain.java
+++ EmployeeWagesMain.java
@@ -3,8 +3,9 @@ package day9.employee_wages_problem;
 public class EmployeeWagesMain {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Welcome to Employee Wage Computation Program...");
+
+		EmployeeWages employeeWages = new EmployeeWages();
+		employeeWages.calculateMonthlyWages();
 
 	}
 
