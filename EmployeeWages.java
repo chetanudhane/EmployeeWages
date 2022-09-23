bba4dabdec0464e42a1b957fb070112b666441bf
chetan prakash udhane <78649010+chetanudhane@users.noreply.github.com>
2022-09-23 21:51:40 +0530

    Wage As Per Condition

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -7,11 +7,13 @@ public class EmployeeWages {
 	public static final int fulltime_Working_Hrs = 8;
 	public static final int parttime_Working_Hrs = 4;
 	public static final int working_Days_For_Month = 20;
+	public static final int total_Working_Hrs = 100;
 
 	public int checkAttendance() {
 
 		int working_Hrs;
 		int empRandomCheck = (int) (Math.random() * 3);
+
 		if (empRandomCheck == emp_Is_Fulltime) {
 			System.out.println("Employee is Present Fulltime");
 			working_Hrs = fulltime_Working_Hrs;
@@ -25,12 +27,11 @@ public class EmployeeWages {
 		return working_Hrs;
 	}
 
-	public int calculateDailyWages() {
+	public int calculateDailyWages(int working_Hrs) {
 
-		int working_Hrs_PerDay = checkAttendance();
 		int wage_Per_Hrs = 20;
 
-		int dailyWage = wage_Per_Hrs * working_Hrs_PerDay;
+		int dailyWage = wage_Per_Hrs * working_Hrs;
 		System.out.println("Daily Wages For Employee Is : " + dailyWage);
 
 		return dailyWage;
@@ -38,12 +39,21 @@ public class EmployeeWages {
 	}
 
 	public void calculateMonthlyWages() {
+		EmployeeWages employeeWages = new EmployeeWages();
 		int monthly_Wage = 0;
+		int working_Hrs_Per_Month = 0;
 		for (int i = 1; i <= working_Days_For_Month; i++) {
 
 			System.out.println("Day-" + i);
+			if (working_Hrs_Per_Month >= total_Working_Hrs) {
+				break;
+			}
 
-			monthly_Wage += calculateDailyWages();
+			int working_Hrs = employeeWages.checkAttendance();
+			int dailyWage = employeeWages.calculateDailyWages(working_Hrs);
+
+			working_Hrs_Per_Month += working_Hrs;
+			monthly_Wage += dailyWage;
 
 		}
 		System.out.println("Monthly Wage Of Employee : " + monthly_Wage);
